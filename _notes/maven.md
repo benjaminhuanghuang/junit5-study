2ae8099a4481f8cbffd300f53df900034ca5f8b3
Benjamin Huang <bhuang@rms.com>
2019-12-12 11:52:59 -0800

    update

--- _notes/maven.md
+++ _notes/maven.md
@@ -1,7 +1,7 @@
 
 
 
-## Maven dependencies
+## Maven plugin for Junit 
 ```   
     <build>
        <plugins>
@@ -13,3 +13,9 @@
        </plugins>
    </build>
 ```
+
+maven-surefire-plugin是个什么鬼
+
+如果你执行过mvn test或者执行其他maven命令时跑了测试用例，你就已经用过maven-surefire-plugin了。
+maven-surefire-plugin是maven里执行测试用例的插件，不显示配置就会用默认配置。
+这个插件的surefire:test命令会默认绑定maven执行的test阶段。
--- junit5-rest-assured/src/test/java/ben/study/PublicRestServicesTest.java
+++ junit5-rest-assured/src/test/java/ben/study/PublicRestServicesTest.java
@@ -1,17 +1,19 @@
-package ben.study.rest;
+package ben.study;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-public class TestPublicRestService {
-     @Test
+public class PublicRestServicesTest {
+    @Disabled("http://echo.jsontest.com/ is over its serving quota from time to time")
+    @Test
     void testEchoService() {
         String key = "foo";
         String value = "bar";
         given().when().get("http://echo.jsontest.com/" + key + "/" + value)
-                .then().assertThat().statusCode(200).body(key,
-                equalTo(value));
+                .then().assertThat().statusCode(200).body(key, equalTo(value));
     }
 
     @Test
